ed22a9b42ad4ec50076718629511437ef11deb57
Ore A <oreadeleye67@gmail.com>
2023-11-10 10:22:15 +0100

    Guard whoami route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,16 +5,19 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { GetUser } from './decorator';
 import { User } from '@prisma/client';
+import { JwtGuard } from './guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @UseGuards(JwtGuard)
   @Get('whoami')
   profile(@GetUser() user: User) {
     return user;
